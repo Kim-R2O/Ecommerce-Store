3c39b591e15b7ba450195f77f4936b8bed066364
Kim <kim@localhost.localdomain>
2021-01-07 19:53:41 -0500

    commit

--- store/views.py
+++ store/views.py
@@ -27,8 +27,23 @@ def cart(request):
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping':False}
+        cartItems = order["get_cart_items"]
+
+        for i in cart:
+            cartItems += cart[i]['quantity']
+
+            product = Product.objects.get(id=i)
+            total = (product.price * cart[i]['quantity'])
+
+            order['get_cart_total'] += total
+            order['get_cart_items'] += cart[i]['quantity']
+
 
     context = {'items':items, 'order': order}
     return render(request, 'store/cart.html', context)
